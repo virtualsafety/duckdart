48a9d46435d800befadc8a0a57e52b8d1d871946
virtualsafety <virtualsafety@users.noreply.github.com>
2024-09-08 15:06:39 +0800

    Create radix.hpp

---
+++ src/include/radix.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <limits.h>
+
+#include <cfloat>
+#include <cstring>  // strlen() on Solaris
+
+#include "bswap.hpp"
+#include "common.hpp"
+#include "exception.hpp"
+
+namespace duckart {
+
+struct Radix {
+   public:
+    static inline bool IsLittleEndian() {
+        int n = 1;
+        if (*char_ptr_cast(&n) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    template <class T>
+    static inline void EncodeData(data_ptr_t dataptr, T value) {
+        throw NotImplementedException("Cannot create data from this type");
+    }
+
+    static inline void EncodeStringDataPrefix(data_ptr_t dataptr,
+                                              string_t value,
+                                              idx_t prefix_len) {
+        auto len = value.GetSize();
+        memcpy(dataptr, value.GetData(), MinValue(static_cast<idx_t>(len), prefix_len));
+        if (len < prefix_len) {
+            memset(dataptr + len, '\0', prefix_len - len);
+        }
+    }
+
+    static inline uint8_t FlipSign(uint8_t key_byte) { return key_byte ^ 128; }
+};
+
+template <>
+inline void Radix::EncodeData(data_ptr_t dataptr, int8_t value) {
+    uint8_t bytes;  // dance around signedness conversion check
+    Store<int8_t>(value, data_ptr_cast(&bytes));
+    Store<uint8_t>(bytes, dataptr);
+    dataptr[0] = FlipSign(dataptr[0]);
+}
+
+template <>
+inline void Radix::EncodeData(data_ptr_t dataptr, int16_t value) {
+    uint16_t bytes;
+    Store<int16_t>(value, data_ptr_cast(&bytes));
+    Store<uint16_t>(BSwap<uint16_t>(bytes), dataptr);
+    dataptr[0] = FlipSign(dataptr[0]);
+}
+
+template <>
+inline void Radix::EncodeData(data_ptr_t dataptr, int32_t value) {
+    uint32_t bytes;
+    Store<int32_t>(value, data_ptr_cast(&bytes));
+    Store<uint32_t>(BSwap<uint32_t>(bytes), dataptr);
+    dataptr[0] = FlipSign(dataptr[0]);
+}
+
+template <>
+inline void Radix::EncodeData(data_ptr_t dataptr, int64_t value) {
+    uint64_t bytes;
+    Store<int64_t>(value, data_ptr_cast(&bytes));
+    Store<uint64_t>(BSwap<uint64_t>(bytes), dataptr);
+    dataptr[0] = FlipSign(dataptr[0]);
+}
+
+template <>
+inline void Radix::EncodeData(data_ptr_t dataptr, uint8_t value) {
+    Store<uint8_t>(value, dataptr);
+}
+
+template <>
+inline void Radix::EncodeData(data_ptr_t dataptr, uint16_t value) {
+    Store<uint16_t>(BSwap<uint16_t>(value), dataptr);
+}
+
+template <>
+inline void Radix::EncodeData(data_ptr_t dataptr, uint32_t value) {
+    Store<uint32_t>(BSwap<uint32_t>(value), dataptr);
+}
+
+template <>
+inline void Radix::EncodeData(data_ptr_t dataptr, uint64_t value) {
+    Store<uint64_t>(BSwap<uint64_t>(value), dataptr);
+}
+
+}  // namespace duckart
